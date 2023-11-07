53d6ed177a3772aaaacb69ed2a3ea73daa9b06ee
GonzaloMerida <gonzalomerida@hotmail.com>
2023-11-07 17:56:31 +0100

    Revisar

--- app/src/main/java/com/example/homeofthedragon/ElectionActivity.kt
+++ app/src/main/java/com/example/homeofthedragon/ElectionActivity.kt
@@ -1,7 +1,10 @@
 package com.example.homeofthedragon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 
 class ElectionActivity : AppCompatActivity() {
@@ -9,19 +12,69 @@ class ElectionActivity : AppCompatActivity() {
     private lateinit var tvGreetingElection : TextView
 
     private lateinit var tvInfoElection : TextView
+    private lateinit var cbRaenira : CheckBox
+    private lateinit var cbAegon : CheckBox
+    private lateinit var bttnHincarRodilla : Button
+
+    companion object {
+        val RESULT_ELECTION = "result"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_election)
 
-        //inicialización del TextView.
+        //inicialización del las vistas.
         tvGreetingElection = findViewById<TextView>(R.id.tvGreetingElection)
+        tvInfoElection = findViewById(R.id.tvInfoEleccion)
+        cbRaenira = findViewById(R.id.cbRaenira)
+        cbAegon = findViewById(R.id.cbAegon)
+        bttnHincarRodilla = findViewById(R.id.bttnHincarRodilla)
         //tomamos el valor del parámetro String que llega con el intent.
         val name = intent.getStringExtra(MainActivity.NAME)
         tvGreetingElection.text = getString(R.string.greeting_election,name)
 
-        tvInfoElection = findViewById<TextView>(R.id.tvGreetingElection)
+
+        //evento del botón hincar la rodilla
+        bttnHincarRodilla.setOnClickListener {
+            val result = getResultFromCheckboxes(cbRaenira.isChecked, cbAegon.isChecked)
+            // Lanza la actividad FinalActivity con el resultado
+            val intent = Intent(this@ElectionActivity, FinalActivity::class.java)
+            intent.putExtra(RESULT_ELECTION, result)
+            startActivity(intent)
+        }
 
 
+        // Eventos de cambio en los checkboxes
+        cbRaenira.setOnCheckedChangeListener { buttonView, isChecked ->
+            updateInfoText()
+        }
+
+        cbAegon.setOnCheckedChangeListener { buttonView, isChecked ->
+            updateInfoText()
+        }
+
+        // Inicializa el texto de información
+        updateInfoText()
+    }
+
+    private fun updateInfoText() {
+        val result = getResultFromCheckboxes(cbRaenira.isChecked, cbAegon.isChecked)
+        tvInfoElection.text = when (result) {
+            "neither" -> getString(R.string.info_neither)
+            "raenira" -> getString(R.string.info_raenira)
+            "aegon" -> getString(R.string.info_aegon)
+            "both" -> getString(R.string.info_both)
+            else -> ""
+        }
+    }
+
+    private fun getResultFromCheckboxes(isRaeniraChecked: Boolean, isAegonChecked: Boolean): String {
+        return when {
+            isRaeniraChecked && isAegonChecked -> "both"
+            isRaeniraChecked -> "raenira"
+            isAegonChecked -> "aegon"
+            else -> "neither"
+        }
     }
 }
--- app/src/main/java/com/example/homeofthedragon/FinalActivity.kt
+++ app/src/main/java/com/example/homeofthedragon/FinalActivity.kt
@@ -1,5 +1,60 @@
 package com.example.homeofthedragon
 
-class FinalActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
+class FinalActivity : AppCompatActivity() {
+    private lateinit var tvResult: TextView
+    private lateinit var bttnIndex: Button
+    private lateinit var bttnBack: Button
+    private lateinit var bttnExit: Button
+
+
+    companion object{
+        const val RESULT_INCOME_ELECTION = "result"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final)
+
+        // Inicialización de vistas
+        tvResult = findViewById(R.id.tvResult)
+        bttnIndex = findViewById(R.id.bttnIndex)
+        bttnBack = findViewById(R.id.bttnBack)
+        bttnExit = findViewById(R.id.bttnExit)
+
+        //recojo el valor del intent pasado desde ElectionActivity y lo guardo
+        //en la variable result
+        val resultElection = intent.getStringExtra(RESULT_INCOME_ELECTION)
+
+        // Actualiza el texto en función del resultado
+        tvResult.text = when (resultElection) {
+            "neither" -> getString(R.string.result_neither)
+            "raenira" -> getString(R.string.result_raenira)
+            "aegon" -> getString(R.string.result_aegon)
+            "both" -> getString(R.string.result_both)
+            else -> ""
+        }
+
+        // Eventos de los botones
+        bttnIndex.setOnClickListener {
+            // Regresar a la actividad principal
+            val intent = Intent(this@FinalActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        bttnBack.setOnClickListener {
+            // Regresar a la actividad ElectionActivity
+            val intent = Intent(this@FinalActivity, ElectionActivity::class.java)
+            startActivity(intent)
+        }
+
+        bttnExit.setOnClickListener {
+            // Cerrar completamente la aplicación
+            finishAffinity()
+        }
+    }
 }
